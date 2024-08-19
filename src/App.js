8826638a2e0aee1fcbdbcb88c5c1462805e5cd39
rrauf8178 <rrauf8178@gmail.com>
2024-08-19 19:52:33 +0400

    add: hero component to app.js initially

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+
+// import components
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="overflow-hidden">
+      <Hero />
+    </div>
+  );
 };
 
 export default App;
