3749918cde72e30df19fff5744adcb959c2d7d09
rrauf8178 <rrauf8178@gmail.com>
2024-08-20 15:10:03 +0400

    add: header with mobile navbar and stats component to home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 // import components
 import Hero from "./components/Hero";
+import Header from "./components/Header";
+import NavMobile from "./components/NavMobile";
+import Stats from "./components/Stats";
 
 // import aos
 import Aos from "aos";
 import "aos/dist/aos.css";
 
 const App = () => {
+  // mobile nav state
+  const [navMobile, setNavMobile] = useState(false);
+
   // aos init
   useEffect(() => {
     Aos.init({ duration: 1000, delay: 400 });
@@ -15,7 +21,13 @@ const App = () => {
 
   return (
     <div className="overflow-hidden">
+      <Header setNavMobile={setNavMobile} />
       <Hero />
+      {/* mobile nav */}
+      <div className={`${navMobile ? 'right-0' : '-right-full'} fixed z-10 top-0 h-full transition-all duration-200`}>
+        <NavMobile setNavMobile = {setNavMobile} />
+      </div>
+      <Stats/>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+// import image
+import Logo from "../assets/img/logo.svg";
+
+// import components
+import Nav from "./Nav";
+import AccountBtns from "./AccountBtns";
+
+// import icons
+import { CgMenuRight } from "react-icons/cg";
+
+const Header = ({setNavMobile}) => {
+  return (
+    <header
+      className="py-[30px] lg:pt-[60px]"
+      data-aos="fade-down"
+      data-aos-delay="900"
+      data-aos-duration="2000"
+    >
+      <div className="container mx-auto flex items-center justify-between">
+        {/* logo */}
+        <a href="#">
+          <img src={Logo} alt="" />
+        </a>
+
+        {/* nav & btns */}
+        <div className="hidden lg:flex gap-x-[55px]">
+          <Nav />
+          <AccountBtns />
+        </div>
+        {/* open nav */}
+        <div className="lg:hidden cursor-pointer" onClick={() => setNavMobile(true)}>
+          <CgMenuRight className="text-2xl" />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
